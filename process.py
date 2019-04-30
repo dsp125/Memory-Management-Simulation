cb462cf63157ad0f712c6fb376b7302ef3b8002b
dsp125 <dpatel0125@gmail.com>
2019-04-30 01:49:46 -0400

    Update process.py

--- process.py
+++ process.py
@@ -10,10 +10,10 @@ PROCESS CLASS DEFINITION for use with project2.py
 class Process(object):
 	def __init__(self):
 		self.name = ""			#Stores the name of the process
-		self.running = False	#True if process running, False otherwise
+		self.running = False		#True if process running, False otherwise
 		self.done = False		#True if process done, False otherwise
 		self.size = 0			#Stores the size of the process
 		self.completed = 0		#Completed bursts
 		self.startTime = 0		#Stores the current start time of the process
-		self.arrivalTimes = []	#Stores process burst arrival times
+		self.arrivalTimes = []		#Stores process burst arrival times
 		self.endTimes = []		#Stores process burst end times
